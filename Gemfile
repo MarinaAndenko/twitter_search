6935771b089e64317a747928b282ff82e905cb5c
MarinaAndenko <marina.andenko@gmail.com>
2017-12-12 23:33:39 +0200

    add frontend to tweets

--- Gemfile
+++ Gemfile
@@ -6,11 +6,13 @@ gem 'jquery-rails'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'rails', '~> 5.0.6'
+gem 'react-rails'
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'twitter'
 gem 'twurl'
 gem 'uglifier', '>= 1.3.0'
+gem 'webpacker'
 
 group :development, :test do
   gem 'byebug', platform: :mri
---
+++ app/javascript/components/Tweets.js
@@ -0,0 +1,140 @@
+import React from "react";
+import PropTypes from "prop-types";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import TextField from 'material-ui/TextField';
+import {orange500} from 'material-ui/styles/colors';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import Badge from 'material-ui/Badge';
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
+
+const enterKey = 13;
+const mixed = 'mixed';
+const recent = 'recent';
+const popular = 'popular';
+const materialStyles = {
+  textField: {
+    color: orange500,
+  },
+};
+
+class Tweets extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      hashtags: {},
+      resultType: null,
+      query: '',
+    };
+  }
+
+  fetchData = (resultType = this.state.resultType, query = this.state.query) => {
+    $.get( '/search', { query: query, result_type: resultType },
+    function(data) {
+      this.setState({
+        results: data.results,
+        hashtags: data.hashtags,
+      })
+    }.bind(this)).fail(function() {});
+  }
+
+  handleChange = (e, value) => {
+    this.setState({ query: value });
+  }
+
+  handleKeyDown =(e) => {
+    if (e.keyCode === enterKey) this.fetchData();
+  }
+
+  handleClick = () => {
+    this.fetchData();
+  }
+
+  handleTypeClick =(e) => {
+    this.setState({ resultType: e.target.textContent });
+    this.fetchData(e.target.textContent);
+  }
+
+  setHashtags = () => {
+    let i = 0, hashtagsArr = [], hashtagsSet = null;
+    let hashtags_hash = this.state.hashtags;
+
+    Object.keys(hashtags_hash).map((key, index) => {
+      hashtagsArr.push(
+        <Badge badgeContent={hashtags_hash[key]} primary={true} key={'hashtag' + i}>
+          <div className='hashtag-div'>
+            #{ key }
+          </div>
+        </Badge>
+      );
+      i++;
+    });
+
+    if(hashtagsArr && hashtagsArr.length)
+      hashtagsSet = <Paper className='hashtags-set' zDepth={2}>{hashtagsArr}</Paper>;
+
+    return hashtagsSet;
+  }
+
+  setResults = () => {
+    let i = 0, resultsArr = [];
+
+    this.state.results.map((result) => {
+      resultsArr.push(
+        <div key={'result' + i}>
+          <div className='result-div'>
+            <b>@{ result.user_name }</b> { result.text }
+          </div>
+          <Divider />
+        </div>
+      );
+      i++;
+    });
+
+    return resultsArr;
+  }
+
+  render () {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <AppBar title="Twitter Search" showMenuIconButton={false} />
+          <div className='search-div'>
+            <div className='text-field-div'>
+              <TextField 
+                hintText="What you want to know?.." hintStyle={materialStyles.textField}
+                fullWidth={true} onChange={this.handleChange} onKeyDown={this.handleKeyDown}
+              />
+            </div>
+            <div className='raised-button-div'>
+              <RaisedButton label="Search" primary={true} className='raised-button' onClick={this.handleClick}/>
+            </div>
+            <div className='text-field-div'>
+              <FlatButton label={mixed} onClick={this.handleTypeClick}/>
+              <FlatButton label={recent} primary={true} onClick={this.handleTypeClick}/>
+              <FlatButton label={popular} secondary={true} onClick={this.handleTypeClick}/>
+            </div>
+          </div>
+          <div className='main-div'>
+            { this.setHashtags() }
+            { this.setResults() }
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+Tweets.propTypes = {
+  results: PropTypes.array,
+  hashtags: PropTypes.object
+};
+
+export default Tweets
